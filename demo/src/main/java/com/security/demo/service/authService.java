a26627c43b036378ff839df3ff087b2af58e14d8
viviana <pastranapastranaviviana@gmail.com>
2025-06-21 21:58:07 -0500

    V1.1.1.1.1

--- demo/src/main/java/com/security/demo/service/authService.java
+++ demo/src/main/java/com/security/demo/service/authService.java
@@ -3,9 +3,11 @@ package com.security.demo.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 import com.security.demo.repository.Iuser;
+import com.security.demo.interfaceService.IUserServices;
 import com.security.demo.model.Users;
 
 
@@ -15,10 +17,25 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 
-public class authService implements  Iuser{
+public class authService implements  IUserServices{
     private final Iuser userRepository;
-    private final jwtServide jwtService;
+    private final jwtService jwtService;
 
 
-    public 
+    public  authResponse register ( registerRequest request) {
+        user userData = User.builder()
+        .first_name(request.getFirstName())
+        .last_name(request.getLastName())
+        .username(request.getUsername())
+        .password(request.getPassword())
+        .build();
+      userRepository.save(userData);
+      return authResponse.builder()
+      .token(jwtService.getToken(userData))
+      .build();
+    }
+
+    public authResponse login(loginRequest request) {
+        return new authResponse(),
+    }
 }
